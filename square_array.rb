00635ae094dccaa13fb4cc524f7f2c2d94ccf75c
Nkenge Simms <simmsn@yahoo.com>
2020-07-23 02:59:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,10 @@
-def square_array(array)
-  array.each  do |array|
-  end
+#def square_array(array)
+#  array.each  do |array|
+ # end
  
+#end
+def square_array(array)
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
 end
